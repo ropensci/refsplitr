9625da5e91596731bbaaf6cf58ff64bc9c1872a1
aurielfournier <aurielfournier@gmail.com>
2018-04-16 20:08:15 -0500

    new extract_country_name function

--- address_parsing_demo.R
+++ address_parsing_demo.R
@@ -1,32 +1,71 @@
-
+library(tidyverse)
+library(refnet)
 library(stringr)
 library(stringi)
 
-dat <- separate(data=final,
-         col = address, 
-         into=c("university","department","address"),
-         sep=",",extra = "merge") %>%
-       mutate(country=stri_extract_last_words(address),
-        zip = str_extract(string=address, 
-          pattern="[:digit:][:digit:][:digit:][:digit:][:digit:]"),
-        city_state = str_extract(string=address,
-                pattern="[:alnum:]{1,20}[,][ ][A-Z][A-Z]") ) %>%
-      select(address, city_state, zip, country)
+
+load("./output/eb_refined.Rdata")
+
+dat <- separate(data=eb_refined, col = address,
+                into=c("university","department","short_address"),
+                sep=",",extra = "merge", remove=FALSE) %>%
+  mutate(country=stri_extract_last_words(short_address),
+         postal_code = str_extract(string=short_address, 
+                      pattern="[:alpha:]{2}[:punct:]{1}[:digit:]{1,8}|[:space:][:upper:][:digit:][:upper:][:space:][:digit:][:upper:][:digit:]|[:alpha:][:punct:][:digit:]{4}")) %>%
+  mutate(postal_code = ifelse(is.na(postal_code), str_extract(string=short_address,
+                      pattern="[:space:][:digit:]{5}"), postal_code)) %>%
+  mutate(postal_code = ifelse(is.na(postal_code), str_extract(string=short_address,
+                      pattern="[:upper:]{1,2}[:alnum:]{1,3}[:space:][:digit:][:alnum:]{1,3}"), postal_code)) %>%
+  select(short_address, postal_code, country)
+           
+
+library(tidytext)
+
+tidydat <- separate(data=eb_refined, col = address,
+                  into=c("university","department","short_address"),
+                  sep=",",extra = "merge", remove=FALSE) %>%
+          select(short_address) %>%
+          mutate(line = 1:nrow(eb_refined)) %>%
+          unnest_tokens(word, short_address)
+          
 
 
-blahblah$city_state <- str_extract(string=blahblah$address,
-            pattern="[:alnum:]{1,20}[,][ ][A-Z][A-Z]")
-
-state_province =  
-
-  
-blahblah <- data.frame(address=dat$address)  
 
 
 
-blahblah$
 
-str_remove(string=dat$address,pattern = "[:upper:][:upper:],")
+
+
+
+
+ifelse(is.na(dat$postal_code), str_extract(string=dat$short_address,
+                                       pattern="[:space:][:digit:]{5}"), dat$postal_code)
+
  
+         city_state = str_extract(string=short_address,
+                          pattern="[[[:alpha:]:space:]:alpha:]{1,30}[,][ ][A-Z][A-Z]")) %>%
+  select(short_address, city_state, postal_code, country)
+
+
+library(rworldmap)
+data(countryExData)
+countries <- countryExData[ , 2] %>%
+        str_trim(side = c("both"))
+
+ss <- str_split(dat$short_address, pattern="[:space:]")
+
+dat <- separate(data=eb_refined, col = address, 
+                into=c("university","department","short_address"),
+                sep=",",extra = "merge", remove=FALSE) %>%
+  mutate(country=str_extract(string=short_address,
+                             pattern=countries),
+         zip = str_extract(string=short_address, 
+                pattern="[:digit:][:digit:][:digit:][:digit:][:digit:]"),
+         city = str_extract(string=short_address,
+                pattern="^.* ")) %>%
+  select(short_address, city, country)
+
+
+ll <- str_split(dat$short_address, pattern=",")
+
   
-blahblah$state <- 
---
+++ refnet/R/extract_country_name.R
@@ -0,0 +1,30 @@
+#' Extracts the country out of a refined author information
+#' 
+#' \code{extract_country_name} This function takes the author list output after the output has been synthesized for incorrect author matches. It contains a similarity score cutoff like read_authors. This however is to further constrain the list. New values ARE NOT created, instead it filters by the sim_score column in the output file. An output file is created using the 'root' argument that specifies the folder/file prefix for the output. The final file will be appended with '_final.csv'. 
+#' 
+#' @param data dataframe from refine_authors
+#' @param address_column name of column in quotes where the addresses are
+extract_country_name <- function(data,
+                                 address_column="address"){
+  # loads in country names
+  load("./data/countries.Rdata")
+  
+  # creates an empty column for country names
+  data$country <- NA
+  
+  # pulls out the address column
+  ad <- data[,address_column]
+
+  for(i in 1:length(countries)){
+    
+    # does the country exist in the character string?
+    dd = str_detect(ad, countries[i])
+    # make it TRUE and FALSE 
+    ee = ifelse(is.na(dd),FALSE, dd)
+    # if TRUE, put in the country name
+    data[ee,"country"] <- countries[i]
+    # repeat!! 
+  }
+  # spit back out the data frame
+  return(data)
+}
