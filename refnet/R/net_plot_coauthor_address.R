69d9ca2946ce87aa993bb04996435b44eacfc3a8
birderboone <birderboone@gmail.com>
2018-05-05 14:38:21 -0700

    degree() agruments have changed
    deleted cmode, its not mode='out'

--- refnet/R/net_plot_coauthor_address.R
+++ refnet/R/net_plot_coauthor_address.R
@@ -153,7 +153,7 @@ products[["plot"]] <- ggplot() +
     geom_point(data = data.frame(layoutCoordinates),
                aes(x = LON, y = LAT), 
                size = 3+100*degree(linkages_points_net, 
-               cmode="outdegree", rescale=TRUE), 
+               mode="out"), 
                pch = 21,
                colour = rgb(8/10, 2/10, 2/10, alpha=5/10), 
                fill = rgb(9/10, 6/10, 6/10, alpha=5/10)
