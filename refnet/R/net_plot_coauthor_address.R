d69362625d3b67391bcad8563de6267a32015111
birderboone <birderboone@gmail.com>
2018-05-05 16:58:54 -0700

    Big update, correctly calculating same addresses

--- refnet/R/net_plot_coauthor_address.R
+++ refnet/R/net_plot_coauthor_address.R
@@ -9,8 +9,8 @@
 #' @param addresses output from the read_addresses() function, containing geocoded address latitude and longitude locations.
 #' @param authors__references output from the read_authors() function, which links author addresses together via AU_ID.
 
-net_plot_coauthor_address <- function(data) {
-
+net_plot_coauthor_address <- function(data,line_resolution=10) {
+  
   # require(Matrix)
   # require(network)
   # require(sna)
@@ -23,21 +23,34 @@ net_plot_coauthor_address <- function(data) {
   
   data <- data[!is.na(data$lon),]
   
-    ##	Link all authors at a particular point location:
-  data$latlon <- paste(data$lat, ",", data$lon, sep="") 
+  ##	Link all authors at a particular point location:
+  data$latlon <- paste0(data$lat, ",", data$lon) 
   
-  data$latlonalpha <- paste0("a",1:nrow(data))
+  #data$latlonalpha <- paste0("a",1:nrow(data))
+  test<-data.frame(latlon=unique(data$latlon))
+  test$LAT<-as.numeric(sapply(strsplit(as.character(test$latlon),','),function(x)x[1]))
+  test$LON<-as.numeric(sapply(strsplit(as.character(test$latlon),','),function(x)x[2]))
+  test$latlonalpha<-paste0('a',1:nrow(test))
   
-
-  linkages <- spMatrix(nrow=length(unique(data$latlonalpha)),
-                       ncol=length(unique(data$UT)),
-                       i = as.numeric(factor(data$latlonalpha)),
-                       j = as.numeric(factor(data$UT)),
-                       x = rep(1, length(data$latlonalpha)) 
+  test1<-merge(test, data[,c('latlon','refID')],by='latlon',all.y=T)
+  #data$latlonalpha <- paste0("a",1:nrow(data))
+  
+  # linkages <- spMatrix(nrow=length(unique(data$latlonalpha)),
+  #                      ncol=length(unique(data$UT)),
+  #                      i = as.numeric(factor(data$latlonalpha)),
+  #                      j = as.numeric(factor(data$UT)),
+  #                      x = rep(1, length(data$latlonalpha)) 
+  # )
+  # 
+  linkages <- spMatrix(nrow=length(unique(test1$latlonalpha)),
+                       ncol=length(unique(test1$refID)),
+                       i = as.numeric(factor(test1$latlonalpha)),
+                       j = as.numeric(factor(test1$refID)),
+                       x = rep(1, length(test1$latlonalpha)) 
   )
   
-  rownames(linkages) <- levels(factor(data$latlonalpha))
-  colnames(linkages) <- levels(factor(data$UT))
+  rownames(linkages) <- levels(factor(test1$latlonalpha))
+  colnames(linkages) <- levels(factor(test1$refID))
   
   ##	Convert to a one-mode representation of countries:
   linkages_points <- linkages %*% t(linkages)
@@ -47,7 +60,7 @@ net_plot_coauthor_address <- function(data) {
   
   
   ##	Or we might be interested in using the network package instead of igraph:
-
+  
   ##	This loads our adjacency matrix into a network object, and we 
   ##		specify directed as FALSE, and because we use the ignore.eval=FALSE
   ##		and names.eval="value" arguments it will load our edge counts in as
@@ -58,27 +71,31 @@ net_plot_coauthor_address <- function(data) {
                                  loops=FALSE, 
                                  ignore.eval=FALSE, 
                                  names.eval="value")
-
+  
   vertex_names <- (linkages_points_net %v% "vertex.names")
   
   ##	Get the world map from rworldmap package:
   world_map <- getMap()
   
-  coords_df <- data.frame("latlonalpha"=vertex_names, 
-          "LON"=as.numeric(data$lon),
-          "LAT"=as.numeric(data$lat),
-          stringsAsFactors=FALSE)
-  
+  # coords_df <- data.frame("latlonalpha"=vertex_names, 
+  #                         "LON"=as.numeric(test1$lon),
+  #                         "LAT"=as.numeric(test1$lat),
+  #                         stringsAsFactors=FALSE)
+  # 
+  # coords_df <- data.frame("latlonalpha"=vertex_names, stringsAsFactors=FALSE)
+  coords_df<-test1
+  #coords_df$LAT<-as.numeric(sapply(strsplit(substr(vertex_names,2,nchar(vertex_names)),','),function(x)x[1]))
+  #coords_df$LON<-as.numeric(sapply(strsplit(substr(vertex_names,2,nchar(vertex_names)),','),function(x)x[2]))
   ##	One could also use ggplot to plot out the network geographically
-
+  
   # Function to generate paths between each connected node
-edgeMaker <- function(whichRow, len = 100, curved = TRUE){
+  edgeMaker <- function(whichRow, len = 100, curved = TRUE){
     
     adjacencyList$rowname <- as.character(adjacencyList$rowname)
     adjacencyList$rownamesA <- as.character(adjacencyList$rownamesA)
     
-    fromC <- layoutCoordinates[layoutCoordinates$latlonalpha==adjacencyList[whichRow,1], 2:3 ]  # Origin
-    toC <- layoutCoordinates[layoutCoordinates$latlonalpha==adjacencyList[whichRow,2], 2:3 ]  # Terminus
+    fromC <- layoutCoordinates[layoutCoordinates$latlonalpha==adjacencyList[whichRow,1], 2:3 ][1,]  # Origin
+    toC <- layoutCoordinates[layoutCoordinates$latlonalpha==adjacencyList[whichRow,2], 2:3 ][1,]  # Terminus
     
     # Add curve:
     graphCenter <- colMeans(layoutCoordinates[,2:3])  # Center of the overall graph
@@ -86,8 +103,8 @@ edgeMaker <- function(whichRow, len = 100, curved = TRUE){
     bezierMid <- (fromC + toC + bezierMid) / 3  # Moderate the Bezier midpoint
     if(curved == FALSE){bezierMid <- (fromC + toC) / 2}  # Remove the curve
     edge <- data.frame(Hmisc::bezier(
-      as.numeric(c(fromC[1], bezierMid[1], toC[1])),  # Generate
-      as.numeric(c(fromC[2], bezierMid[2], toC[2])),  # X & y
+      as.numeric(c(fromC[2], bezierMid[2], toC[2])),  # Generate
+      as.numeric(c(fromC[1], bezierMid[1], toC[1])),  # X & y
       evaluation = len))  # Bezier path coordinates
     edge$Sequence <- 1:len  # For size and colour weighting in plot
     edge$Group <- paste(adjacencyList[whichRow, 1:2], collapse = ">")
@@ -96,19 +113,19 @@ edgeMaker <- function(whichRow, len = 100, curved = TRUE){
   
   adjacencyMatrix <- as.matrix(linkages_points)
   
-  layoutCoordinates <- coords_df
+  layoutCoordinates <- test
   
   adjacencydf <- data.frame(adjacencyMatrix) %>%
     rownames_to_column() %>%
     gather(rownamesA, value, -rowname)   
-
-    adjacencyList <- adjacencydf[adjacencydf$value > 0, ]
+  
+  adjacencyList <- adjacencydf[adjacencydf$value > 0, ]
   
   
   # Generate a (curved) edge path for each pair of connected nodes
   allEdges <- lapply(1:nrow(adjacencyList), 
                      edgeMaker, 
-                     len = 500, 
+                     len = line_resolution, 
                      curved = TRUE)
   
   
@@ -132,10 +149,10 @@ edgeMaker <- function(whichRow, len = 100, curved = TRUE){
   world_map@data$id = rownames(world_map@data)
   world_map.points <- fortify(world_map)
   world_map.df <- full_join(world_map.points, world_map@data, by="id")
-
-products <- list()
   
-products[["plot"]] <- ggplot() + 
+  products <- list()
+  
+  products[["plot"]] <- ggplot() + 
     geom_polygon(data=world_map.df, 
                  aes(long,lat,group=group), 
                  fill=gray(8/10)) +
@@ -144,31 +161,31 @@ products[["plot"]] <- ggplot() +
               color=gray(6/10)) +
     coord_equal() + 
     geom_path(data=allEdges,
-                 aes(x = x, y = y, 
-                 group = Group,  # Edges with gradient
-                 colour = Sequence, 
-                 size = -Sequence), 
+              aes(x = x, y = y, 
+                  group = Group,  # Edges with gradient
+                  colour = Sequence, 
+                  size = -Sequence), 
               alpha=0.1
-  )   + 
+    )   + 
     geom_point(data = data.frame(layoutCoordinates),
                aes(x = LON, y = LAT), 
                size = 3+100*sna::degree(linkages_points_net, 
-                                   cmode="outdegree",rescale=F), 
+                                   cmode="outdegree",rescale=T), 
                pch = 21,
                colour = rgb(8/10, 2/10, 2/10, alpha=5/10), 
                fill = rgb(9/10, 6/10, 6/10, alpha=5/10)
-  )   + 
+    )   + 
     scale_colour_gradient(low = rgb(8/10, 2/10, 2/10, alpha=5/10), 
                           high = rgb(8/10, 2/10, 2/10, alpha=5/10), 
                           guide = "none") + 
     scale_size(range = c(5/10, 5/10), guide = "none") + # Customize taper
-  empty_theme
-
+    empty_theme
   
-products[["data_path"]] <- allEdges
-products[["data_polygon"]] <- world_map.df
-products[["data_points"]] <- data.frame(layoutCoordinates)
-
+  
+  products[["data_path"]] <- allEdges
+  products[["data_polygon"]] <- world_map.df
+  products[["data_points"]] <- data.frame(layoutCoordinates)
+  
   return(products)
 }
 
