71a532364e6ec16fcedef85c4b30063fcc15e54c
birderboone <birderboone@gmail.com>
2018-07-07 11:20:25 -0700

    Removing address parsing, addding errors

--- refnet/R/address_lat_long.R
+++ refnet/R/address_lat_long.R
@@ -14,59 +14,60 @@ address_lat_long <- function(data,
                              filename_root="",
                              write_out_missing=FALSE){
   # Read in the CSV data and store it in a variable 
-  # Parse out address information into its respective parts
-  origAddress <- separate(data=data, col = address_column,
-                          into=c("university","department","short_address"),
-                          sep=",",extra = "merge", remove=FALSE) %>%
-    mutate(postal_code = str_extract(string=short_address, 
-                                     pattern="[:alpha:]{2}[:punct:]{1}[:digit:]{1,8}|[:space:][:upper:][:digit:][:upper:][:space:][:digit:][:upper:][:digit:]|[:alpha:][:punct:][:digit:]{4}")) %>%
-    mutate(postal_code = ifelse(is.na(postal_code), 
-                                str_extract(string=short_address,
-                                            pattern="[:space:][:digit:]{5}"), postal_code)) %>%
-    mutate(postal_code = ifelse(is.na(postal_code), 
-                                str_extract(string=short_address,
-                                            pattern="[:upper:]{1,2}[:alnum:]{1,3}[:space:][:digit:][:alnum:]{1,3}"),
-                                postal_code))
+  paste.frame<-data[,c('state','university','city','country','postal_code','authorID','address')]
+  paste.frame$state[is.na(paste.frame$state)]<-''
+  paste.frame$city[is.na(paste.frame$city)]<-''
+  paste.frame$university[is.na(paste.frame$university)]<-''
+  paste.frame$country[is.na(paste.frame$country)]<-''
+  paste.frame$postal_code[is.na(paste.frame$postal_code)]<-''
   
+  #ddd<-subset(final,address!='Could not be extracted')
   
-  origAddress <- extract_country_name(origAddress)
-  
-  #pastes a short address together and replaces NAs with '' for the api search, which doesnt enjoy NAs
-  
-  origAddress$paste_address <- paste(ifelse(is.na(origAddress$country),'',origAddress$country), ifelse(is.na(origAddress$postal_code),'',origAddress$postal_code))
-  
-  #create a dataframe of addresses and link them using the 'adID' key.
-  uniquead<-data.frame(short_address=unique(origAddress$short_address))
-  uniquead$adID<-1:nrow(uniquead)
-  uniquead$short_address<-as.character(uniquead$short_address)
-  origAddress<-merge(origAddress,uniquead, by='short_address',all.x=T)
-  
+  #make short address
+  # with university
+  # paste.frame$short_address[paste.frame$address!='Could not be extracted']<-paste0(paste.frame$university[paste.frame$address!='Could not be extracted'],', ',paste.frame$city[paste.frame$address!='Could not be extracted'],', ',paste.frame$state[paste.frame$address!='Could not be extracted'],', ',paste.frame$postal_code[paste.frame$address!='Could not be extracted'],' ',paste.frame$country[paste.frame$address!='Could not be extracted'])
+  # 
+  #without university
+  paste.frame$short_address[paste.frame$address!='Could not be extracted']<-paste0(paste.frame$city[paste.frame$address!='Could not be extracted'],', ',paste.frame$state[paste.frame$address!='Could not be extracted'],', ',paste.frame$postal_code[paste.frame$address!='Could not be extracted'],' ',paste.frame$country[paste.frame$address!='Could not be extracted'])
+  #grepl(',  ,',paste.frame$short_address[1])
+  paste.frame$short_address<-gsub(', ,|,,|,  ,',', ',paste.frame$short_address)
+  paste.frame$short_address<-trimws(paste.frame$short_address,which='both')
+  paste.frame$short_address<-as.character(paste.frame$short_address)
+  uniqueAddress<-data.frame(short_address=unique(paste.frame$short_address),lat=NA,lon=NA,stringsAsFactors=F)
+  uniqueAddress<-uniqueAddress[!is.na(uniqueAddress),]
+  uniqueAddress$adID<-1:nrow(uniqueAddress)
+  proc.time()-ptm
   #add lat long for later calculation
-  uniquead$lat<-NA
-  uniquead$lon<-NA
   # Loop through the addresses to get the latitude and longitude of each address and add it to the
   # We're using the data science toolkit first because it has no maximum queery limits. 
   # The remainder of addresses will be thrown into googles api
-  
-  for(i in uniquead$adID){
-    #i<-uniquead$adID[1]
-    address<-uniquead$short_address[uniquead$adID==i]
+  check.open<-NA
+  check.open<-tryCatch(scan('http://www.datasciencetoolkit.org',nlines=1),warning=function(w){w})
+  if(!is.na(check.open[1])){print('data science toolkit is down right now, moving onto google API')}
+  if(is.na(check.open[1])){
+  for(i in 1:nrow(uniqueAddressdress)){
+    
+    address<-as.character(uniqueAddressdress$short_address[i])
     print(paste("Working... ",address))
     
     if(!is.na(address)){
+      
+    
       suppressWarnings(result <- geocode(address, output = "latlona", 
                                          source = "dsk",
                                          messaging = TRUE))
-      uniquead$lat[i]<-result[1]
-      uniquead$lon[i]<-result[2]
-    
+      uniqueAddressdress$lat[i]<-result[1]
+      uniqueAddressdress$lon[i]<-result[2]
+    #if(is.na(result[1])){error.counter<-error.counter+1}else{error.counter<-1}
+      #if(error.counter==10){break;print('Data science tooklit seems to be down or your internet is out, trying google api')}
     }
     # We can delete the clock if it's just not working visually
-    total <- nrow(uniquead)
+    total <- nrow(uniqueAddressdress)
     pb <- txtProgressBar(min = 0, max = total, style = 3)
     setTxtProgressBar(pb, i)
     flush.console()
   }
+    }
   
   # Now we'll run the failed addresses into googles api.
   # 1. The keys here are theres a maximum of 2500 querries per day
@@ -80,34 +81,35 @@ address_lat_long <- function(data,
 
   retry<-T
   while(retry==T){
-    faileddsk <- uniquead[is.na(uniquead$lat),c('short_address','adID')]
+    faileddsk <- uniqueAddress[is.na(uniqueAddress$lat),c('short_address','adID')]
     warn.list<-list()
-  for(p in 1:nrow(faileddsk)){
-    #p<-1
-    paste_address<-origAddress$paste_address[faileddsk$adID[p]==origAddress$adID][1]
-    result<-tryCatch(geocode(paste_address, 
-            output = "latlona", 
-            source="google"),warning=function(w){w})
-    if(!is.data.frame(result)){warn.list[[paste0(p)]]<-result;next}
-    result$adID<-faileddsk$adID[p]
-    uniquead$lat[uniquead$adID==result$adID]<-result$lat
-    uniquead$lon[uniquead$adID==result$adID]<-result$lon
+    for(p in 1:nrow(faileddsk)){
+      #p<-1
+      paste_address<-uniqueAddress$short_address[faileddsk$adID[p]==uniqueAddress$adID][1]
+      result<-tryCatch(geocode(paste_address, 
+                               output = "latlona", 
+                               source="google"),warning=function(w){w})
+      if(!is.data.frame(result)){warn.list[[paste0(p)]]<-result;next}
+      result$adID<-faileddsk$adID[p]
+      uniqueAddress$lat[uniqueAddress$adID==result$adID]<-result$lat
+      uniqueAddress$lon[uniqueAddress$adID==result$adID]<-result$lon
+      
+    }
+    # check if all addresses ran or were stopped by a sever limit
+    retry<-sum(grepl('OVER_QUERY_LIMIT',warn.list))>0
+    if(nrow(faileddsk)==1 & paste_address==' '){retry<-F}
+    # we need to cehck if the 2500 limit is being reached. Hopefully this never happens.
     
+    if(geocodeQueryCheck(userType = "free")==0){retry<-F;print("You've run out of server queries today. Max is 2500. Try again tomorrow with a subsetted data set to finish addresses.")}
+    
+    #put system to sleep for 5 seconds to allow googles query limits to reset
+    if(retry==T){print('server busy, trying again in 5 seconds');Sys.sleep(5)}
   }
-  # check if all addresses ran or were stopped by a sever limit
-  retry<-sum(grepl('OVER_QUERY_LIMIT',warn.list))>0
-  if(nrow(faileddsk)==1 & paste_address==' '){retry<-F}
-  # we need to cehck if the 2500 limit is being reached. Hopefully this never happens.
-  
-  if(geocodeQueryCheck(userType = "free")==0){retry<-F;print("You've run out of server queries today. Max is 2500. Try again tomorrow with a subsetted data set to finish addresses.")}
-  
-  #put system to sleep for 5 seconds to allow googles query limits to reset
-  if(retry==T){print('server busy, trying again in 5 seconds');Sys.sleep(5)}
-  }
- 
+  c('authorID','AF','groupID','author_order','address','university','department','RP_address','RI','OI','UT','refID','postal_code','country','lat','lon')%in%colnames(addresses)
   # merge results together
-  addresses<-merge(origAddress,uniquead[,c('adID','lat','lon')],by='adID',all.x=T)
-  addresses<-subset(addresses,select=c(authorID,author_name,groupID,author_order,address,university,department,RP_address,RI,OI,UT,refID,postal_code,country,lat,lon,adID))
+  addresses<-merge(uniqueAddress,subset(paste.frame,select=-address),by='short_address',all.x=T)
+  addresses<-merge(addresses, subset(data,select=c(authorID,AF,groupID,author_order,address,department,RP_address,RI,OI,UT,refID)),by='authorID',all.x=T)
+  addresses<-subset(addresses,select=c(authorID,AF,groupID,author_order,address,university,department,RP_address,RI,OI,UT,refID,postal_code,country,lat,lon))
 
   missingaddresses <- addresses[is.na(addresses$lat),]
   
