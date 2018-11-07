ceea87aa807c2998e2a0fe94617b2a3c8662dbb5
aurielfournier <aurielfournier@gmail.com>
2018-11-06 20:22:04 -0600

    typos...ugh

--- tests/testthat/test_authors_clean.R
+++ tests/testthat/test_authors_clean.R
@@ -5,7 +5,7 @@ test_that("Output of clean_authors makes sense", {
 df<-data.frame(filename=NA, AB=NA , AF=c('Smith, Jon J.','Thompson, Bob B.','Smith,J'), AU=c('Smith, Jon J.','Thompson, Bob','Smith, J'), BP=NA , C1=c("Univ Florida, Gainesville, FL USA","University of Texas, Austin, TX, USA",NA),CR=NA,DE=NA, DI=NA, EM=c("j.smith@ufl.edu",NA,'jsmith@usgs.gov'), EN=NA, FN=NA, FU=NA, PD=NA, PG=NA, PT=NA, PU=NA, PY=NA, RI=NA, OI=NA,PM=NA, RP=c("Univ Florida, Gainesville, FL USA","University of Texas, Austin, TX, USA",NA), SC=NA, SN=NA, SO=NA, TC=NA, TI=NA, UT=NA, VL=NA, WC=NA,Z9=NA,refID=c(1,2,3) ,stringsAsFactors=F)
 df[]<-lapply(df, as.character)
 
-actual <- authors_clean(refereces=df,sim_score)
+actual <- authors_clean(references=df,sim_score)
 expect_is(actual, 'list')
 expect_is(actual$prelim, 'data.frame')
 expect_is(actual$review, 'data.frame')
