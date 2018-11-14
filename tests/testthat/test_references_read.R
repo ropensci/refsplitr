46e10053a6194df4c1f059faecc0d252836db327
aurielfournier <aurielfournier@gmail.com>
2018-11-14 06:19:45 -0600

    write csv test for referneces read

--- tests/testthat/test_references_read.R
+++ tests/testthat/test_references_read.R
@@ -1,7 +1,9 @@
 context("Reading References")
 
 test_that("Read references, reads correctly", {
-d<-references_read(data='../extdata/PubExample.txt' ,dir=F)
+d<-references_read(data='../extdata/PubExample.txt' ,dir=F,
+                   filename_root = "rr_test")
+expect(file.exists("rr_test_references.csv"))
 expect_equal(nrow(d),10)
 expect_equal(ncol(d),32)
 expect_false(any(is.na(d$AF)))
