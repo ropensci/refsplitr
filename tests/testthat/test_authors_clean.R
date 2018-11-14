0d4a2bc49755780be3a5c01c8ac908065089ba1b
aurielfournier <aurielfournier@gmail.com>
2018-11-14 05:59:30 -0600

    authors refine and authors refine tests

--- tests/testthat/test_authors_clean.R
+++ tests/testthat/test_authors_clean.R
@@ -6,10 +6,13 @@ df<-data.frame(filename=NA, AB=NA , AF=c('Smith, Jon J.','Thompson, Bob B.',
     'Smith,J'), AU=c('Smith, Jon J.','Thompson, Bob','Smith, J'), BP=NA ,
 C1=c("Univ Florida, Gainesville, FL USA","University of Texas, Austin, TX, USA",
     NA),CR=NA,DE=NA, DI=NA, EM=c("j.smith@ufl.edu",NA,'jsmith@usgs.gov'),
-EN=NA, FN=NA, FU=NA, PD=NA, PG=NA, PT=NA, PU=NA, PY=NA, RI=NA, OI=NA,PM=NA, 
+EN=NA, FN=NA, FU=NA, PD=NA, PG=NA, PT=NA, PU=NA, PY=NA,
+RI=c("Smith,Jon/H-9999-2012","Thompson,Bob/H-2222-2012",
+     "Smith,Jon/H-2769-2012"), OI=NA,PM=NA, 
 RP=c("Univ Florida, Gainesville, FL USA","University of Texas, Austin, TX, USA",
      NA), SC=NA, SN=NA, SO=NA, TC=NA, TI=NA, UT=NA, VL=NA, WC=NA,Z9=NA,
 refID=c(1,2,3) ,stringsAsFactors=F)
+
 df[]<-lapply(df, as.character)
 
 actual <- authors_clean(references=df,sim_score)
@@ -20,5 +23,25 @@ expect_gt(nrow(actual$review),0)
 expect_gt(nrow(actual$prelim),0)
 expect_equal(actual$prelim$groupID,c(3,2,3))
 expect_gt(min(actual$prelim$similarity,na.rm=T),0.88)
+
+
+
+df<-data.frame(filename=NA, AB=NA , AF=c('Smith, Jon J.','Thompson, Bob B.',
+                                         'Smith,J'), AU=c('Smith, Jon J.','Thompson, Bob','Smith, J'), BP=NA ,
+               C1=c("Univ Florida, Gainesville, FL USA","University of Texas, Austin, TX, USA",
+                    NA),CR=NA,DE=NA, DI=NA, EM=c("j.smith@ufl.edu",NA,'jsmith@usgs.gov'),
+               EN=NA, FN=NA, FU=NA, PD=NA, PG=NA, PT=NA, PU=NA, PY=NA,
+               RI=c("Smith,Jon/H-9999-2012","Thompson,Bob/H-2222-2012",
+                    "Smith,Jon/H-2769-2012"), OI=NA,PM=NA, 
+               RP=c("Univ Florida, Gainesville, FL USA","University of Texas, Austin, TX, USA",
+                    NA), SC=NA, SN=NA, SO=NA, TC=NA, TI=NA, UT=NA, VL=NA, WC=NA,Z9=NA,
+               refID=c(1,2,3) ,stringsAsFactors=F)
+
+df[]<-lapply(df, as.character)
+
+actual <- authors_clean(references=df,sim_score, write_out_data = TRUE)
+
+expect(file.exists("_authors_prelim.csv"))
+expect(file.exists("_authors_review.csv"))
 })
 
--- tests/testthat/test_authors_refine.R
+++ tests/testthat/test_authors_refine.R
@@ -63,4 +63,42 @@ df.review<-data.frame("authorID"=c(1,3),
   expect_equal(df.prelim[is.na(df.prelim$similarity) | 
                         df.prelim$similarity>0.88,'authorID'],actual$authorID)
   
+  
+  df.prelim<-data.frame("authorID"=c(1,2,3),
+                        "AU"= c('Smith, Jon J.','Thompson, Bob B.','Smith,J'),          
+                        "AF"= c('Smith, Jon J.','Thompson, Bob B.','Smith,J'),          
+                        "groupID"= c(3,2,3),      
+                        "match_name"=c('Smith,J',NA,'Smith, Jon J'),
+                        "similarity"= c(0.8833333,NA,0.8833333),   
+                        "author_order"= c(1,1,1),
+                        "address"=c("Univ Florida, Gainesville, FL USA",
+                                    "University of Texas, Austin, TX, USA",NA),      
+                        "RP_address"=c("Univ Florida, Gainesville, FL USA",
+                                       "University of Texas, Austin, TX, USA",NA),   
+                        "RI"=NA,          
+                        "OI"=NA,      
+                        "EM"=c("j.smith@ufl.edu",NA,'jsmith@usgs.gov'),          
+                        "refID"=c(1,2,3),
+                        "TA"=NA,
+                        "SO"=NA,          
+                        "UT"=NA,
+                        "PT"=NA,
+                        "PU"=NA,
+                        "PY"=NA,
+                        "university"=c('Univ Florida','University of Texas',NA),  
+                        "country"=c('USA','USA',NA),
+                        "state"=c('FL','TX',NA),
+                        "postal_code"=NA,
+                        "city"=c('Gainesville','Austin',NA),
+                        "department"=NA , stringsAsFactors=F )
+  
+  df.review2<-as.data.frame(matrix(ncol=ncol(df.review), nrow=0))
+
+  colnames(df.review2) <- colnames(df.review)
+
+  expect_output(authors_refine(df.review2, df.prelim, sim_score=0.88),"Authors data.frame is empty. \n           This likely means there are no authors that need to be handchecked, \n                              outputting the prelim file.")
+  
+
+  
+  
 })
