fa656daf4b8ea9a01b510456e805952770f689f8
birderboone <birderboone@gmail.com>
2018-11-17 15:06:27 -0500

    test5

--- tests/testthat/test_plots.R
+++ tests/testthat/test_plots.R
@@ -38,7 +38,7 @@ test_that("Plotting Addresses works", {
                  stringsAsFactors=FALSE )
   a<-ggplot(df, aes(x=lon,y=lat)) +geom_point()
   a
-  vdiffr::expect_doppelganger('testplot',a,verbose=T)
+  vdiffr::expect_doppelganger('testplot',a)
   
  #  a<-plot_addresses_points(data=df)
  # vdiffr::expect_doppelganger("test_plot_addresses",a)
