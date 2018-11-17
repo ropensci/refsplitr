4641e6256c1723f796d85ee8790ddf620ecd53ca
birderboone <birderboone@gmail.com>
2018-11-17 17:49:45 -0500

    updating netplot tests

--- tests/testthat/test_plots.R
+++ tests/testthat/test_plots.R
@@ -1,6 +1,6 @@
 context("test plot addresses")
 
-test_that("Plotting Addresses works", {
+test_that("Map plots work", {
   
 
   require(ggplot2)
@@ -48,10 +48,45 @@ test_that("Plotting Addresses works", {
 0 codes from your data failed to match with a country code in the map
      failedCodes failedCountries
 241 codes from the map weren't represented in your data")
- 
+})
+
+ test_that("Net plots work", {
+   
+   
+   require(ggplot2)
+   df<-data.frame("authorID"=c(1,2,3),
+                  "AU"= c('Smith, Jon J.','Thompson, Bob B.','Smith,J'),          
+                  "AF"= c('Smith, Jon J.','Thompson, Bob B.','Smith,J'),          
+                  "groupID"= c(3,2,3),      
+                  "match_name"=c('Smith,J',NA,'Smith, Jon J'),
+                  "similarity"= c(0.8833333,NA,0.8833333),   
+                  "author_order"= c(1,2,1),
+                  "address"=c("Univ Florida, Gainesville, FL USA",
+                              "University of Texas, Austin, TX, USA",NA),      
+                  "RP_address"=c("Univ Florida, Gainesville, FL USA",
+                                 "University of Oxford, Oxfordshire, UK",
+                                 "University of California Berkley, Berkley, CA, USA"),   
+                  "RI"=NA,          
+                  "OI"=NA,      
+                  "EM"=c("j.smith@ufl.edu",NA,'jsmith@usgs.gov'),          
+                  "refID"=c(1,1,3),
+                  "TA"=NA,
+                  "SO"=NA,          
+                  "UT"=c('test1','test1','test2'),
+                  "PT"=NA,
+                  "PU"=NA,
+                  "PY"=NA,
+                  "university"=c('Univ Florida','University of Oxford',
+                                 "University of California"),  
+                  "country"=c('USA','United Kingdom',"USA"),
+                  "state"=c('FL','Oxfordshire',"CA"),
+                  "postal_code"=NA,
+                  "city"=c('Gainesville','Oxford',"Berkley"),
+                  "department"=NA ,
+                  "lat"=c(35,51.7520,39),
+                  "lon"=c(-100,1.2577,-80),
+                  stringsAsFactors=FALSE )
  c<-plot_net_address(data=df)
- 
- expect_output(plot_net_address(data=df),regexp='Regions defined for each Polygons')
  c$data_points$latlon
  expect_equal(c$data_points$latlon, factor(c("35,-100","51.752,1.2577" ,"39,-80")))
  expect_equal(length(c$data_polygon$long),12576)
@@ -60,8 +95,10 @@ test_that("Plotting Addresses works", {
  expect_equal(d[1],c("United Kingdom"=0,"USA"=1))
  expect_equal(d[2],c("United Kingdom"=1,"USA"=0))
  
-  # vdiffr::expect_doppelganger("test_plot_net_coauthor",
-  # plot_net_coauthor(data=df1))
+ e<-plot_net_country(data=df)
+ expect_equal(signif(e$data_points$LAT,4),c(53.88, 39.50))
+ expect_equal(length(e$data_polygon$long),12576)
+  
 })
   
 
