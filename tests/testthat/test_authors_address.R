ae2015bddff0d916d79d95f8b2636630092a75ea
birderboone <mttboone@gmail.com>
2019-04-13 12:16:52 -0400

    added new unit test for USA addresses

--- tests/testthat/test_authors_address.R
+++ tests/testthat/test_authors_address.R
@@ -9,15 +9,18 @@ address=c("Univ Sydney, Fac Vet Sci, Sch Life & Environm Sci,
   "Ryerson Univ, Dept Chem & Biol, Toronto, ON M5B 2K3, Canada.",
   "Univ Sao Paulo, Inst Biociencias, Dept Zool, BECO, Rua Matao,Trav 14 321,
   BR-05508090 Sao Paulo, Brazil.",
-  "Univ Sydney, Charles Perkins Ctr, Sydney, NSW, Australia."),
-  authorID=1:7,stringsAsFactors=FALSE)
+  "Univ Sydney, Charles Perkins Ctr, Sydney, NSW, Australia.",
+  "niv Florida, IFAS, Ft Lauderdale, FL 33312 USA."),
+  authorID=1:8,stringsAsFactors=FALSE)
 
 actual<-authors_address(df$address, df$authorID)
 expect_false(any(is.na(actual$country)))
-expect_false(grepl('BR',actual$postal_code[actual$country=='Brazil']))
-expect_equal(sum(grepl("[A-Z]{1}[0-9]{1}[A-Z]{1}\\s[0-9]{1}[A-Z]{1}[0-9]{1}",
+expect_false(grepl('BR',actual$postal_code[actual$country=='brazil']))
+expect_equal(sum(grepl("[a-z]{1}[0-9]{1}[a-z]{1}\\s[0-9]{1}[a-z]{1}[0-9]{1}",
                        actual$postal_code)),2)
 expect_equal(unique(actual$country[grepl(
-  "[A-Z]{1}[0-9]{1}[A-Z]{1}\\s[0-9]{1}[A-Z]{1}[0-9]{1}", 
-  actual$postal_code)]),'Canada')
+  "[a-z]{1}[0-9]{1}[a-z]{1}\\s[0-9]{1}[a-z]{1}[0-9]{1}", 
+  actual$postal_code)]),'canada')
+expect_equal(c(actual$country[8], actual$state[8], actual$postal_code[8]),
+             c('usa','fl','33312'))
 })
